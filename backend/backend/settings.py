c0720c5580e72811eead20bd1f6fa4aaba36049c
Naol Mitiku <naolmitiku@gmail.com>
2024-09-01 01:59:25 -0700

    add cos

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -43,12 +43,16 @@ INSTALLED_APPS = [
     'django_filters',
     'users',
     'treatment',
-    'appointment'
+    'appointment',
+
+    'corsheaders',
 ]
 
 AUTH_USER_MODEL = 'users.User'
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -167,3 +171,8 @@ EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER') + ""
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD') + ""
+
+
+CORS_ALLOWED_ORIGINS = [
+   "*"
+]
