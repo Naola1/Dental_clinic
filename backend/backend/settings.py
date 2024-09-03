46433915f7e22b9e51a7d25be51236a66f96389f
Naol Mitiku <naolmitiku@gmail.com>
2024-09-03 05:40:36 -0700

    db migration to postgres

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -86,10 +86,21 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'lilydb',
+        'USER': 'lilydb_user',
+        'PASSWORD': 'rfjA6hwbjVLhF0MnHAMQ8I3qVDqpbwbS',
+        'HOST': 'dpg-crbfbk3v2p9s73dganqg-a.oregon-postgres.render.com',
+        'PORT': '5432',
     }
 }
 
@@ -173,6 +184,3 @@ EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER') + ""
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD') + ""
 
 CORS_ALLOW_ALL_ORIGINS = True
-CORS_ALLOWED_ORIGINS = [
-   "*"
-]
